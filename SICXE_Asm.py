65d974f6fbbffe4dcfaf98a44b21bb0abb39b6c8
Omar El Araby <79846026+rogitson@users.noreply.github.com>
2021-11-28 21:02:10 +0200

    Improved efficiency

--- SICXE_Asm.py
+++ SICXE_Asm.py
@@ -1,5 +1,4 @@
-inputFile = "in.txt"
-instructionFile = "Instruction_Set.txt"
+# import sys, getopt
 
 #For formatting the strings
 def format(str):
@@ -20,63 +19,67 @@ def readFile(File,Array):
         col[2] = col[2].rstrip("\n")
         Array.append(col)
 def passOne():
+    if(codearr[0][1].upper() != "START"):
+        raise Exception("A very unspecific bad thing happened, but I won't tell you what it is.") 
     locFile=open("out.txt","w")
     litable = open("litTable.txt", "w")
     lit = []
     start_address=int(codearr[0][2], 16)
-    start_address=hex(start_address) 
+    current_address=hex(start_address) 
     for i in codearr:
-        locFile.write(start_address + '\n')
-        # print(start_address,"\t\t",i[1])
+        locFile.write(current_address + '\n')
+        # print(current_address,"\t\t",i[1])
+        steps = 0
         if(i[1] == "LTORG" or i[1] == "END"):
             for e in lit:
                 if(e[1] == ''):
-                    e[1] = start_address
+                    steps = 0
+                    e[1] = current_address
                     if(e[0][1]=="C"):
                         for z in e[0][2:]:
                             if(z != "'"):
-                                start_address = hex(int(start_address,16) + 1)
+                                steps += 1
                     else:
-                        start_address = hex(int(start_address,16) + 1)  
-                    litable.write(e[0] + "\t" + e[1] + "\n")   
+                        steps += 1
+                    current_address = hex(int(current_address,16) + steps)  
+                    litable.write(e[0] + "\t" + e[1] + "\n") 
+            continue
+        elif(i[1] == "BASE"):
+            continue  
         elif(i[1][0] == "+"):
-            start_address = hex(int(start_address,16) + 4)
+            steps += 4
         elif(i[1][0] == "&"):
-            start_address = hex(int(start_address,16) + 3)
+            steps += 3
         elif(i[1][0] == "$"):
-            start_address = hex(int(start_address,16) + 4)
+            steps += 4
         elif(i[1]=="WORD"):
             data = i[2].split(",")
             for j in data:
-                start_address = hex(int(start_address,16) + 3)
+                steps += 3
         elif(i[1]=="BYTE"):
             data = i[2].split(",")
             for j in data:
                 if(j[0]=="C"):
                     for z in j[1:]:
                         if(z != "'"):
-                            start_address = hex(int(start_address,16) + 1)
+                            steps += 1
                 else:
-                    start_address = hex(int(start_address,16) + 1)
+                    steps += 1
         elif(i[1]=="RESW"):
-            value = hex(int(i[2]))
-            value = value[2:]
-            start_address = hex(int(start_address,16) + int(value,16) * 3)
+            steps += int(i[2]) * 3
         elif(i[1]=="RESB"):
-            value = hex(int(i[2]))
-            value = value[2:]
-            start_address = hex(int(start_address,16) + int(value,16))
+            steps += int(i[2])
         for j in insarr:
             if(i[1]==j[0]):
                 if(i[1] == "RSUB"):
                     if(i[2] != ''):
                         raise Exception("A very specific bad thing happened, but I won't tell you what it is.")
                 if(j[1]=="1"):
-                    start_address = hex(int(start_address,16) + 1)
+                    steps += 1
                 elif(j[1]=="2"):
-                    start_address = hex(int(start_address,16) + 2)
+                    steps += 2
                 elif(j[1]=="34"):
-                    start_address = hex(int(start_address,16) + 3)
+                    steps += 3
         if(i[2] != '' and i[2][0] == "="):
             flag = True
             for e in lit:
@@ -84,6 +87,7 @@ def passOne():
                     flag = False
             if(flag):
                 lit.append([i[2],''])
+        current_address = hex(int(current_address,16) + steps)
     locFile.close()
     litable.close()
     symbol_table()
@@ -101,18 +105,45 @@ def symbol_table():
     symbFile.close()
     locFile.close()
 
-code = open(inputFile, "r")
-ins = open(instructionFile, "r")
-codearr = []
-insarr = []
-        
-readFile(code,codearr)   
-readFile(ins,insarr)
+# def main(argv):
+#     try:
+#         opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+#     except getopt.GetoptError:
+#         print ('test.py -i <inputfile> -o <outputfile>')
+#         sys.exit(2)
+#     for opt, arg in opts:
+#         if opt in ('-h', "--help"):
+#             print("-h, --help\t-\tprint the help message\n" +
+#             "-i, --input\t-\tinput file\n" +
+#             "-s, --set\t-\tinstruction set file\n" +
+#             "-o, --output\t-\toutput file")
+#             sys.exit()
+#         elif opt in ("-i", "--input"):
+#             inputfile = arg
+#         elif opt in ("-s", "--set"):
+#             instructionFile = arg
+#         elif opt in ("-o", "--output"):
+#             outputfile = arg
 
-passOne()
+if __name__ == "__main__":
+    inputFile = "in.txt"
+    instructionFile = "in_set.txt"
+    # inputFile = ''
+    # instructionFile = ''
+    # main(sys.argv[1:])
 
-code.close()
-ins.close()
+    code = open(inputFile, "r")
+    ins = open(instructionFile, "r")
+    codearr = []
+    insarr = []
+            
+    readFile(code,codearr)   
+    readFile(ins,insarr)
+
+    passOne()
+    print("Success!")
+    code.close()
+    ins.close()
 
 def hex_to_decimal(number):
     number=int(hex(number).split('x')[-1])
